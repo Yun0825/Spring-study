f5b7485e080c0d4f40f5e54634b78da33dd2454e
HYEONGYUN\guddb <guddbs5545@naver.com>
2024-08-25 18:26:02 +0900

    App config 리팩터링

--- core/core/src/main/java/hello/core/MemberApp.java
+++ core/core/src/main/java/hello/core/MemberApp.java
@@ -7,7 +7,10 @@ import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appconfig = new AppConfig();
+        MemberService memberService = appconfig.memberService();
+
+      //  MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
